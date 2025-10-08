5f6bca7c8d11d5996040c5f77b30c9c42b24dd9f
PAVEL AKSENOV <pavelaksenov10@gmail.com>
2025-10-08 18:09:33 +0300

    update 1: the 'i forgot to commit this'

--- mainprojectfile/ingame.cpp
+++ mainprojectfile/ingame.cpp
@@ -1,19 +1,64 @@
 #include <iostream>
 using namespace std;
-int main() {
-    int temp;
-    // clear screen
-    for (int i = 0; i < 50; i++) {
-        cout << endl;
+void clear() {
+    for (int i = 0; i < 50; i++) cout << endl;
+}
+void attributemenu(int& strength, int& agility, int& intelligence) {
+    clear();
+    cout << "======================================" << endl;
+    cout << "      welcome to the rpg!!! you have loaded in (thankfully)      " << endl;
+    cout << "======================================" << endl;
+    cout << endl;
+    cout << "starting attributes:"<< endl;
+    cout << endl;
+    cout << "   .----------------------------.   " << endl;
+    cout << "   |  [1] strength   (warrior)  |   " << endl;
+    cout << "   |  [2] agility    (rogue)    |   " << endl;
+    cout << "   |  [3] intelligence (mage)   |   " << endl;
+    cout << "   '----------------------------'   " << endl;
+    cout << endl;
+    cout << "enter your choice (1-3): ";
+    int choice;
+    cin >> choice;
+    switch (choice) {
+        case 1:
+            strength = 10; agility = 5; intelligence = 3;
+            cout << "you chose strength!" << endl;
+            break;
+        case 2:
+            strength = 5; agility = 10; intelligence = 3;
+            cout << "you chose agility!"<< endl;
+            break;
+        case 3:
+            strength = 3; agility = 5; intelligence = 10;
+            cout << "you chose intelligence!" << endl;
+            break;
+        default:
+            cout << "invalid choice! choosing strength instead" << endl;
+            strength = 10; agility = 5; intelligence = 3;
+            break;
     }
-    cout << "==========================" << endl;
-    cout << "      game started!       " << endl;
-    cout << "==========================" << endl;
+    cout << "press enter to continue...";
+    cin.ignore();
+    cin.get();
+}
+
+int main() {
+    int strength = 0, agility = 0, intelligence = 0;
+    attributemenu(strength, agility, intelligence);
+    clear();
+    cout << "==============================" << endl;
+    cout << "   you're in a dark spooky forest " << endl;
+    cout << "==============================" << endl;
     cout << endl;
-    cout << "you are in the game world!" << endl;
-    cout << "your adventure begins here..." << endl;
+    cout << "attributes: " << endl;
+    cout << "strength:     " << strength << endl;
+    cout << "agility:      " << agility << endl;
+    cout << "intelligence: " << intelligence << endl;
     cout << endl;
-    cout << "enter any number to go back to menu: ";
+    cout << "your rpg adventure is beninging..." << endl;
+    cout << "(конец кода пока что введи любую цифру чтобы вернуться в меню): ";
+    int temp;
     cin >> temp;
     return 0;
 }
